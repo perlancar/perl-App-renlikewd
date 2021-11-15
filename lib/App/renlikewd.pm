c2d40002ce4b6844358af4d665b48d0d268d336e
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-11-15 18:14:31 +0700

    Initial commit

---
+++ lib/App/renlikewd.pm
@@ -0,0 +1,26 @@
+package App::renlikewd;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: REName a file so it becomes LIKE the current (Working) Directory's name
+
+=head1 SYNOPSIS
+
+See the command-line script L<renlikewd>.
+
+
+=head1 SEE ALSO
+
+L<perlmv> (from L<App::perlmv>)
+
+L<renwd> (from L<App::renwd>)
+
+L<App::RenameUtils>
+
+=cut
